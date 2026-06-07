e369b673f26a3e73acc131a43b36531029bc340d
joanwarukira254 <joanwarukira30@gmail.com>
2026-06-08 00:34:56 +1000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,41 @@
+Filename:  main.tf
+ 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+ 
+provider "azurerm" {
+  features {}
+}
+ 
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+ 
+resource "azurerm_storage_account" "main" {
+  name                     = "acu612webapp"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+ 
+  static_website {
+    index_document = "index.html"
+  }
+}
+ 
+resource "azurerm_storage_blob" "index" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.main.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  source                 = "index.html"
+  content_type           = "text/html"
+}
+ 
