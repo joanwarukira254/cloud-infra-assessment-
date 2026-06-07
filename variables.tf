a2360b600d729cec4d177d162f4e6957e7348f0e
joanwarukira254 <joanwarukira30@gmail.com>
2026-06-08 00:38:57 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Azure region for all resources"
+  default     = "Australia East"
+}
+ 
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  default     = "assessment3-rg"
+}
